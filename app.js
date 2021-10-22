1d0ed24769a30e50ac757c917262f79f979c0a4d
Annely Kurg <annely.kurg@khk.ee>
2021-10-22 16:12:45 +0300

    Add task delete functionality

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 //event elements
 const form = document.querySelector("form");
+const taskList = document.querySelector(".collection");
+
 //events
 // selectors ei ole vaja kirjutada. Sulud, jutumärk FORM ja tuleb selectors automaatselt.
 form.addEventListener("submit", addTask);
+taskList.addEventListener("click", deleteTask);
+
+function deleteTask(event) {
+    if(event.target.textContent === "X") {
+        if(confirm("Do you want to delete this task?")) {
+            event.target.parentElement.remove();
+        }
+    }
+}
 
 function addTask(event){
     //event = e Ei pea pikalt välja kirjutama
@@ -16,6 +27,16 @@ function addTask(event){
     const text = document.createTextNode(task);
     //add txt to li item
     li.appendChild(text);
+    //create a element
+    const link = document.createElement("a");
+    //add css class
+    link.className = "secondary-content";
+    //set href atribute to a
+    link.setAttribute("href", "#");
+    //add text content to a
+    link.appendChild(document.createTextNode("X"));
+    //add a to li
+    li.appendChild(link);
     //add li item to ul
     const ul = document.querySelector(".collection");
     ul.appendChild(li);
