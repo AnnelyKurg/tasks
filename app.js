340df2fd4a564cd931c4ad8f581fa9126736f5b4
Annely Kurg <annely.kurg@khk.ee>
2021-11-05 07:44:40 +0200

    Delete single task value from localStorage

--- app.js
+++ app.js
@@ -21,8 +21,26 @@ function deleteTask(event) {
     if(event.target.textContent === "X") {
         if(confirm("Do you want to delete this task?")) {
             event.target.parentElement.remove();
+            task = event.target.parentElement.firstChild.textContent;
+            deleteTaskFromLocalStorage(task)
         }
     }
+}
+
+function deleteTaskFromLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem("tasks") === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+    tasks.forEach(function (tasksElement, index) {
+        if (tasksElement === task) {
+            tasks.splice(index, 1);
+        }
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function addTask(event) {
@@ -69,10 +87,10 @@ function addTaskToLocalStorage(task) {
     } else {
         tasks = JSON.parse(localStorage.getItem("tasks"));
     }
-    console.log(tasks);
+
     tasks.push(task);
     localStorage.setItem("tasks", JSON.stringify(tasks));
-    console.log(tasks);
+
 
 }
 
